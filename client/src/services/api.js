6bbde2e581bd2d673c235b8702688cd0833ec21f
Marcelo Bonilla <marcelokanu@gmail.com>
2020-07-27 12:59:48 -0400

    service to deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api/',
+  baseURL:
+    'http://localhost:3001/api/' ||
+    'https://mbonilla-igti-final.herokuapp.com/api/',
 });
 
 export default api;
