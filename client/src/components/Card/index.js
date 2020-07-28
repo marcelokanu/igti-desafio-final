1bd686c5b5a31c1c366d41be75dd334193c46c08
Marcelo Bonilla <marcelokanu@gmail.com>
2020-07-28 18:25:01 -0400

    countup added

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import CountUp from 'react-countup';
 import { CardContainer, Card } from './styles';
 import { formatMoney, sumValues } from '../../util';
 
 export default function BoxInfos(props) {
   const { data: filteredTransactions } = props;
   const [calcValues, setCalcValues] = useState({
+    oldIncome: 0,
     income: 0,
     outcome: 0,
-    saldo: 0,
+    oldOutcome: 0,
+    balance: 0,
+    oldbalance: 0,
   });
 
   useEffect(() => {
@@ -18,12 +22,22 @@ export default function BoxInfos(props) {
     const outcomeValues = filteredTransactions.filter(({ type }) => {
       return type === '-';
     });
+    const oldIncome = calcValues.income;
+    const oldOutcome = calcValues.outcome;
+    const oldBalance = calcValues.balance;
 
     const income = sumValues(incomeValues);
     const outcome = sumValues(outcomeValues);
-    const saldo = income - outcome;
+    const balance = income - outcome;
 
-    setCalcValues({ income, outcome, saldo });
+    setCalcValues({
+      oldIncome,
+      income,
+      oldOutcome,
+      outcome,
+      oldBalance,
+      balance,
+    });
   }, [filteredTransactions]);
 
   return (
@@ -33,21 +47,58 @@ export default function BoxInfos(props) {
           <p>Receitas</p>
           <i className="material-icons">trending_up</i>
         </header>
-        <h1>{formatMoney(calcValues.income)}</h1>
+        <h1>
+          <CountUp
+            preserveValue={true}
+            start={calcValues.oldIncome}
+            end={calcValues.income}
+            duration={1}
+            separator="."
+            decimals={2}
+            decimal=","
+            prefix="R$ "
+            delay={100}
+            useEasing={true}
+          />
+        </h1>
       </Card>
       <Card type="despesa">
         <header>
           <p>Despesas</p>
           <i className="material-icons">trending_down</i>
         </header>
-        <h1>{formatMoney(calcValues.outcome)}</h1>
+        <h1>
+          <CountUp
+            preserveValue={true}
+            start={calcValues.oldOutcome}
+            end={calcValues.outcome}
+            duration={1}
+            separator="."
+            decimals={2}
+            decimal=","
+            prefix="R$ "
+            useEasing={true}
+          />
+        </h1>
       </Card>
       <Card type="saldo" total>
         <header>
           <p>Saldo</p>
           <i className="material-icons">import_export</i>
         </header>
-        <h1>{formatMoney(calcValues.saldo)}</h1>
+        <h1>
+          <CountUp
+            preserveValue={true}
+            start={calcValues.oldBalance}
+            end={calcValues.balance}
+            duration={1}
+            separator="."
+            decimals={2}
+            decimal=","
+            prefix="R$ "
+            useEasing={true}
+          />
+        </h1>
       </Card>
     </CardContainer>
   );
